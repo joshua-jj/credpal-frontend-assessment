5f5d76b555b722348bf7dfd391d4ef727b0e25f5
Joshua Akhidenor <akhidenorjoshua@gmail.com>
2025-04-14 19:17:22 +0100

    Clean up code

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,4 @@
 "use client";
-import customersIcon from "@/assets/icons/customers.svg";
-import helpIcon from "@/assets/icons/help.svg";
-import logoutIcon from "@/assets/icons/logout.svg";
-import marginOrdersIcon from "@/assets/icons/margin-orders.svg";
-import notificationIcon from "@/assets/icons/notification.svg";
-import overviewIcon from "@/assets/icons/overview.svg";
-import settingsIcon from "@/assets/icons/settings.svg";
-import spotOrdersIcon from "@/assets/icons/spot-orders.svg";
-import transactionsIcon from "@/assets/icons/transactions.svg";
-import walletIcon from "@/assets/icons/wallet.svg";
 import isAuth from "@/components/isAuth";
 import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
